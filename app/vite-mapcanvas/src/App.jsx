ae344063911fba6355551adf938d14c43c13cfab
aPuppyGod <tfuenot12321@gmail.com>
2026-01-20 17:56:27 +0000

    Create App.jsx

---
+++ app/vite-mapcanvas/src/App.jsx
@@ -0,0 +1,16 @@
+"import React from 'react'
+import { Toaster } from 'sonner'
+import MapCanvas from './pages/MapCanvas'
+import './App.css'
+
+function App() {
+  return (
+    <div className=\"App\">
+      <MapCanvas />
+      <Toaster position=\"bottom-center\" theme=\"dark\" />
+    </div>
+  )
+}
+
+export default App
+"
