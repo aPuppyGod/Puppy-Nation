ee962d3f53d730a2480007998df112e7b140e66d
aPuppyGod <tfuenot12321@gmail.com>
2026-01-20 20:48:39 +0000

    Update vite.config.js

--- app/vite-mapcanvas/vite.config.js
+++ app/vite-mapcanvas/vite.config.js
@@ -1,20 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Puppy-Nation/',
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: false,
-  },
-  server: {
-    port: 3000,
-  },
-})
+  base: "/Puppy-Nation/"
+});
