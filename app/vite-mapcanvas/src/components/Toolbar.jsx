380579cc22a636737ea9d1d57e49d06d5cbf95c2
aPuppyGod <tfuenot12321@gmail.com>
2026-01-20 17:59:00 +0000

    Create Toolbar.jsx

---
+++ app/vite-mapcanvas/src/components/Toolbar.jsx
@@ -0,0 +1,26 @@
+"import React from 'react'
+
+const Toolbar = ({ tools, selectedTool, onSelectTool }) => {
+  return (
+    <div className=\"toolbar-container\">
+      <div className=\"glass-pill flex flex-col gap-1 p-2\">
+        {tools.map((tool) => {
+          const Icon = tool.icon
+          return (
+            <button
+              key={tool.id}
+              onClick={() => onSelectTool(tool.id)}
+              className={`tool-btn ${selectedTool === tool.id ? 'active' : ''}`}
+              title={tool.label}
+            >
+              <Icon size={20} />
+            </button>
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default Toolbar
+"
